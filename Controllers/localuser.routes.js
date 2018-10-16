c98e85dd6be7f472017ef939b500c8384f77a59a
Kevin Toledo <kgtoledoc@unal.edu.co>
2018-10-16 14:42:34 -0500

    Changes

--- Controllers/localuser.routes.js
+++ Controllers/localuser.routes.js
@@ -1,10 +1,12 @@
 const Router = require('express').Router();
 const User = require('../Model/user.model');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
 
 Router.post('/new', (req, res) => {
   let newuser = new User;
   newuser.username= req.body.username;
-  newuser.password= req.body.password;
+  newuser.password= bcrypt.hashSync(req.body.password, salt);
   newuser.save((err, newuser) => {
     if(newuser){
       req.session.localUser = newuser;
@@ -15,12 +17,16 @@ Router.post('/new', (req, res) => {
 Router.post('/login', (req, res) => {
   User.find({
     username: req.body.username,
-    password: req.body.password,
+    //password: req.body.password,
   }, (err, theuser) => {
-    if(theuser) {
+    if(bcrypt.compareSync(req.body.password, theuser[0].password)){
       req.session.localUser = theuser;
       res.redirect('/homepage');
+    } else {
+      res.redirect('/');
     }
+      //req.session.localUser = theuser;
+      //res.redirect('/homepage');
   })
 })
 
